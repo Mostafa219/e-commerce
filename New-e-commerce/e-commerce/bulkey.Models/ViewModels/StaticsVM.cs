f27b328fc603e364219d55e40b53255b316ebb5f
mostafa arafat <mostafaarafat@gmail.com>
2022-05-28 15:46:32 +0200

    e-commerce website

---
+++ New-e-commerce/e-commerce/bulkey.Models/ViewModels/StaticsVM.cs
@@ -0,0 +1,29 @@
+﻿
+using bulkey.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bulkey.Models.ViewModels
+{
+    public class StaticsVM
+    {
+        public IEnumerable<Product> Product { get; set; }
+
+        public IEnumerable<Compan> Compan { get; set; }
+        public IEnumerable<CoverType> CoverType { get; set; }
+
+        public IEnumerable<ApplicationUser> ApplicationUser { get; set; }
+
+        public IEnumerable<OrderHeader> OrderHeaders { get; set; }
+
+        public IEnumerable<Category> Category { get; set; }
+
+
+    }
+}
+
+
+
